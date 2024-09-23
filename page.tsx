25faf0b335803ed9e505f337f32052b2f990bd85
AliJazib4 <159694599+AliJazib4@users.noreply.github.com>
2024-09-23 12:01:35 +0500

    Add files via upload

---
+++ page.tsx
@@ -0,0 +1,12 @@
+import Header from "./Header";
+import About from "./About";
+
+export default function Home() {
+  return(
+<>
+  <h1>This is Home Page</h1>
+<Header/>
+<About/>
+</>     //fragments
+);
+}
